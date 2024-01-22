847b202d132d4402e40bd7ed1fff07adf53c5406
Dailylulll <94639602+Dailylulll@users.noreply.github.com>
2024-01-22 13:13:45 -0600

    basic file structure and workflow

---
+++ visuals/display.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+'''Add title for what it is, caption for training material'''
+def train_v_test(trainX, trainY, testX, testY):
+  plt.figure(figsize=(10, 8))
+  plt.scatter(trainX, trainY, c="b", s=4, label="Training data")
+  plt.scatter(testX, testY, c="r", s=4, label="Testing data")
+  plt.legend()
+  plt.show()
